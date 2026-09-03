9256c4bc4e4db358e6deafc8e182f809c3ecc68e
Gonzalo8a69 <101830338+Gonzalo8a69@users.noreply.github.com>
2026-09-02 22:53:35 -0500

    Change sidebar background color from dark to light

--- interfaz.py
+++ interfaz.py
@@ -25,7 +25,7 @@ def aplicar_estilos():
         section[data-testid="stSidebar"], 
         section[data-testid="stSidebar"] > div:first-child,
         section[data-testid="stSidebar"] > div:first-child > div { 
-            background-color: #0B1630 !important; 
+            background-color: #6D7383 !important; 
         }
         section[data-testid="stSidebar"] {
             border-right: 1px solid #2A3F5F !important; 
